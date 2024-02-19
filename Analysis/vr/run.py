4bd226cb4547f513823780e01011552b52ab86c9
sarasilvamonteiro <140457374+sarasilvamonteiro@users.noreply.github.com>
2024-02-19 16:23:13 +0000

    Update run.py

--- Analysis/vr/run.py
+++ Analysis/vr/run.py
@@ -1,28 +1,25 @@
-from dataset import *
-from jan24.draw_umap import draw_umap
+from jan_24.draw_umap_2 import *
 #%%
-ManageData()
+data_manager = ManageData(Windows=True)
+features = data_manager.load_features('hands')
+
 #%%
-# load csv
-hands_syllables = ManageData().load_syllables('hands')
-surface_syllables = ManageData().load_syllables('surface')
-preprocess_hands = ManageData().preprocess(hands_syllables)
-umap_values = ManageData().extract_values(preprocess_hands, as_array=False)
+hands_syllables = data_manager.load_syllables('hands')
+surface_syllables = data_manager.load_syllables('surface')
+preprocess_hands = data_manager.load_preprocessed_syllables('hands')
+#%%
+n = 5
+d = 0.1
+draw_umap(features.iloc[:, :-100], n_components=2,n_neighbors=n, min_dist=d, hand='Right', holes=1, pca_components=10)
+draw_umap(features.iloc[:, :-100], n_components=2,n_neighbors=n, min_dist=d, hand='Right', holes=2, pca_components=10)
+draw_umap(features.iloc[:, :-100], n_components=2,n_neighbors=n, min_dist=d, hand='Left', holes=1, pca_components=10)
+draw_umap(features.iloc[:, :-100], n_components=2,n_neighbors=n, min_dist=d, hand='Left', holes=2, pca_components=10)
+
 
 #%%
 n = 5
 d = 0.1
-draw_umap(n_components=2,n_neighbors=n, min_dist=d, hand='Right', holes=1)
-draw_umap(n_components=2,n_neighbors=n, min_dist=d, hand='Right', holes=2)
-draw_umap(n_components=2,n_neighbors=n, min_dist=d, hand='Left', holes=1)
-draw_umap(n_components=2,n_neighbors=n, min_dist=d, hand='Left', holes=2)
-
-
-#%%
-
-
-
-
+draw_umap_by_trial(features, n_components=2,n_neighbors=n, min_dist=d, hand='Right', holes=1)
 
 
 
