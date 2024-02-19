cafc0a3d480d8f45a17d89e39f7556fd5683ac5d
sarasilvamonteiro <140457374+sarasilvamonteiro@users.noreply.github.com>
2024-02-19 09:48:03 +0000

    Add files via upload

--- Analysis/vr/draw_umap_2.py
+++ Analysis/vr/draw_umap_2.py
@@ -2,23 +2,16 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import umap
 import pandas as pd
-from jan_24.dataset_2 import *
+from jan24.dataset_2 import *
+from sklearn.decomposition import PCA
+
 
 data_manager = ManageData(IOS=True)
 
+# last_update: 15 Feb 24
 
 def draw_umap(umap_values, hand, holes, trial='all', n_neighbors=15, min_dist=0.1, n_components=2, lr=1, metric='euclidean',
-              title=''):  # 'euclidean'
-
-    #sort_by = 'time_interval'
-    #data = umap_values.sort_values(by=sort_by)
-    # get syllables in data (sorted by^'____________')
-    #data_syllables = umap_values.index.get_level_values('syllable')
-    # get int and str labels for each unique syllable
-    #int_labels , str_labels = pd.factorize(data_syllables.unique())
-    # create a dictionary between them
-    #labels_dict =  dict(zip(str_labels, int_labels))
-    #print(labels_dict)
+              title='', pca=False):  # 'euclidean'
 
     suptitle = f'Hand: {hand.lower()} Trial: {str(trial)} Holes: {str(holes)}'#  (sorted by {sort_by})'
     title = f'(neigh:{str(n_neighbors)} min-dist:{str(min_dist)})'
@@ -30,56 +23,70 @@ def draw_umap(umap_values, hand, holes, trial='all', n_neighbors=15, min_dist=0.
         metric=metric,
         learning_rate=lr)
 
+    pca_ = PCA(n_components=10)
+
     if trial != 'all':
         trial_data = umap_values.loc[:, :, :, trial, holes, :, hand]
     else: # trial == all
         trial_data = umap_values.loc[:, :, :, :, holes, :, hand]
 
-    # get from the label dictionary the int_label for the syllables in this trial
-    #trial_labels = [labels_dict[key] for key in trial_data.index.get_level_values('syllable')]
 
     trial_syllables = trial_data.index.get_level_values('syllable')
+    print(trial_syllables)
     section_cmap = []
     up = []
     down = []
     for idx, syllable in enumerate(trial_syllables):
+        #if type(syllable) != type('str'):
+        #    LCR, BMF = 'L','F'
+        print(trial_data.iloc[idx])
         if syllable[1] in ['F', 'M', 'B']:
             LCR = syllable[0]
-            print(LCR)
             BMF = syllable[1]
-            print(BMF)
         if syllable[1] not in ['F', 'M', 'B']:
             LCR = syllable[0:2]
-            print(LCR)
             BMF = syllable[2]
-            print(BMF)
 
         section_cmap.append(data_manager.section_cmap(LCR, BMF, length=100))
 
+        #if type(syllable) != type('str'):
+        #    continue
         if syllable[-2:] == 'Up':
             up.append(idx)
         if syllable[-4:] == 'Down':
             down.append(idx)
 
-    u = fit.fit_transform(trial_data)
+
+
+    if pca:
+        pca_vals = pca_.fit_transform(trial_data)
+        u = fit.fit_transform(pca_vals)
+    else:
+        u = fit.fit_transform(trial_data)
 
     fig = plt.figure(figsize=(10, 5))
     gs = gridspec.GridSpec(1, 2, width_ratios=[2*n_components, 2/n_components], height_ratios=[1])
+
+    #print(len(section_cmap), len(up), len(down), len(u), len(trial_data.iloc[up+down]))
+    color = ['black', 'silver']
 
     if n_components == 2:
         # plot UMAP
         ax1 = plt.subplot(gs[0])
         ax1.set_title(title)
-        ax1.scatter(u[:, 0][up], u[:, 1][up], c=[section_cmap[i] for i in up], marker=r'$\Uparrow$', s=200)
-        ax1.scatter(u[:, 0][down], u[:, 1][down], c=[section_cmap[i] for i in down], marker=r'$\Downarrow$', s=200)
-        ax1.plot(u[:, 0],u[:, 1], linewidth=0.4, c='k')
+        ax1.scatter(u[:, 0][up], u[:, 1][up], c=color[0], marker=r'$\Uparrow$', s=200)
+        ax1.scatter(u[:, 0][down], u[:, 1][down], c=color[1], marker=r'$\Downarrow$', s=200)
+
+        #ax1.scatter(u[:, 0][up], u[:, 1][up], c=[section_cmap[i] for i in up], marker=r'$\Uparrow$', s=200)
+        #ax1.scatter(u[:, 0][down], u[:, 1][down], c=[section_cmap[i] for i in down], marker=r'$\Downarrow$', s=200)
+        #ax1.plot(u[:, 0],u[:, 1], linewidth=0.4, c='k')
         ax1.set_aspect('auto')
     if n_components == 3:
         ax1 = plt.subplot(gs[0], projection='3d')
         ax1.set_title(title)
         ax1.scatter(u[:, 0][up], u[:, 1][up], c=[section_cmap[i] for i in up], s=100, marker=r'$\Uparrow$')
         ax1.scatter(u[:, 0][down], u[:, 1][down], c=[section_cmap[i] for i in down], marker=r'$\Downarrow$')
-        ax1.plot(u[:, 0], u[:, 1], linewidth=0.4, c='k')
+        #ax1.plot(u[:, 0], u[:, 1], linewidth=0.4, c='k')
         ax1.set_aspect('auto')
 
     # plot 2D cmap
@@ -98,14 +105,6 @@ def draw_umap(umap_values, hand, holes, trial='all', n_neighbors=15, min_dist=0.
 
 def draw_umap_by_trial(umap_values, hand, holes, trial='all', n_neighbors=15, min_dist=0.1, n_components=2, lr=1, metric='euclidean',
               title=''):  # 'euclidean'
-
-    # get syllables in data (sorted by^'____________')
-    #data_syllables = umap_values.index.get_level_values('syllable')
-    # get int and str labels for each unique syllable
-    #int_labels , str_labels = pd.factorize(data_syllables.unique())
-    # create a dictionary between them
-    #labels_dict =  dict(zip(str_labels, int_labels))
-    #print(labels_dict)
 
     suptitle = f'Hand: {hand.lower()} Trial: {str(trial)} Holes: {str(holes)}'#  (sorted by {sort_by})'
     title = f'(neigh:{str(n_neighbors)} min-dist:{str(min_dist)})'
@@ -141,9 +140,6 @@ def draw_umap_by_trial(umap_values, hand, holes, trial='all', n_neighbors=15, mi
             if len(trial_data) <= n_neighbors+1:
                 print('next', len(trial_data))
                 continue
-
-            # get from the label dictionary the int_label for the syllables in this trial
-            #trial_labels = [labels_dict[key] for key in trial_data.index.get_level_values('syllable')]
 
             trial_syllables = trial_data.index.get_level_values('syllable')
             section_cmap = []
